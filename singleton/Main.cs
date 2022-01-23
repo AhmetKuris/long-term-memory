2b1c9ed7a0e3668d5be9ade163dc82c42878490c
Ahmet Kuris <ahmetkuris2308@gmail.com>
2022-01-24 00:17:35 +0100

    topic : add singleton sample and definition

---
+++ singleton/Main.cs
@@ -0,0 +1,39 @@
+﻿public class Main
+{
+	public Main()
+	{
+		public void Example()
+        {
+            var singletonSample = SingletonSample.Instance;
+        }
+	}
+}
+
+public sealed class SingletonSample
+{
+    private static SingletonSample instance;
+    private static readonly object lockObject = new object();
+
+    //Private constructor so that other classes can't create another one
+    private SingletonSample()
+    {
+    }
+
+    public static SingletonSample Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonSample();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
