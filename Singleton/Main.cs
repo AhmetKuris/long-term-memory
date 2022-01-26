6c7fa380fa21f5eceb95e032a33d46bf3a76930e
Ahmet Kuris <ahmetkuris2308@gmail.com>
2022-01-26 15:04:58 +0100

    fix: add missing parts

---
+++ Singleton/Main.cs
@@ -0,0 +1,39 @@
+﻿namespace Singleton
+{
+    public class Main
+    {
+        public Main()
+        {
+            var singletonSample = SingletonSample.Instance;
+        }
+    }
+
+    public sealed class SingletonSample
+    {
+        private static SingletonSample instance;
+        private static readonly object lockObject = new object();
+
+        //Private constructor so that other classes can't create another one
+        private SingletonSample()
+        {
+        }
+
+        public static SingletonSample Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SingletonSample();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
